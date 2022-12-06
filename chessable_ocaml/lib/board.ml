63529cb581b0427089716c91af099f5c9a2decfc
Zhengping <tony.jiang.zhengping@gmail.com>
2022-12-06 17:01:01 -0500

    implemeting board

--- chessable_ocaml/lib/board.ml
+++ chessable_ocaml/lib/board.ml
@@ -4,7 +4,7 @@ open Core
 
 type location = (char * int) [@@deriving eq, sexp];;
 
-type direction = N | NE | E | SE | S | SW | W | NW;;
+type direction = N | NE | E | SE | S | SW | W | NW [@@deriving eq];;
 
 type meta = Pawn of {ep: bool} | King of {ck: bool;cq: bool} | Other;;
 
@@ -14,20 +14,6 @@ type piece =
    location:location;
    meta:meta}
 ;;
-
-type square =
-    | Empty of location
-    | Occupied of location * piece;;
-
-type plain_board = square list list;;
-
-
-type board = {
-  board: plain_board;
-  white_pieces: piece list;
-  black_pieces: piece list;
-  side_to_play: Parser.side;
-}
 
 let col_of_int (i: int): char =
   match i with
@@ -52,6 +38,53 @@ let col_to_int (r: char): int =
   | 'g' -> 7
   | 'h' -> 8
   | _ -> failwith "Invalid column indicator"
+
+
+type square =
+    | Empty of location
+    | Occupied of location * piece;;
+
+type plain_board = square list list;;
+
+let get_square (board: plain_board) (c: char) (r: int): square =
+  List.nth_exn (List.nth_exn board ((col_to_int c) - 1)) (r - 1)
+ 
+let set_square (board: plain_board) (square: square): plain_board =
+  (* Get information is square *)
+  let lc, lr = match square with
+    | Empty loc -> loc
+    | Occupied (loc, _) -> loc
+  in
+  let rec set_board_pos (ic: int) (ir: int) (square: square) (board: plain_board): plain_board =
+    if ic > 1 then
+      match board with
+      | h::tl -> h::(set_board_pos (ic - 1) ir square tl)
+      | [] -> board
+    else
+      let rec set_square_list_pos (pos: int) (square: square) (sl: square list): square list =
+        if pos > 1 then
+          match sl with
+          | h::tl -> h::(set_square_list_pos (pos - 1) (square) tl)
+          | [] -> sl
+        else
+          match sl with
+          | _::tl -> square::tl
+          | [] -> sl
+      in
+      match board with
+      | h::tl ->
+        (set_square_list_pos ir square h)::tl
+      | [] -> board
+  in
+  let lci = col_to_int lc in
+  set_board_pos lci lr square board
+
+type board = {
+  board: plain_board;
+  white_pieces: piece list;
+  black_pieces: piece list;
+  side_to_play: Parser.side;
+}
 
 let initialize: board =
   (* This function will initialize a board to a playable state *)
@@ -294,7 +327,7 @@ let check_no_piece_interference (mv: Parser.mv) (piece: piece) (board: plain_boa
               | None -> failwith "This location should be valid but failed!"
               | Some (c, r) -> 
                 begin
-                  match List.nth_exn (List.nth_exn board (col_to_int c)) r with
+                  match get_square board c r with
                   | Empty _ -> true
                   | Occupied _ -> false
                 end
@@ -302,6 +335,122 @@ let check_no_piece_interference (mv: Parser.mv) (piece: piece) (board: plain_boa
             let checked_results = List.init ~f:(gen_loc_exn d (pc, pr)) (abs (row - pr) - 1) in
             List.fold ~init:true ~f:(fun agg -> (fun x -> agg && x)) checked_results
         end
+    end
+
+let check_no_discovered_check (mv: Parser.mv) (piece: piece) (board: board): bool =
+  (* 
+     We are not checking checks from knight, because knight checks cannot be discovered
+     The strategy is: identify the line (diagnal / row / col) defined by the piece and the side king.
+     (If without that piece on that line no piece interferes a opponent checkable piece, then false)
+  *)
+  match piece.piece with
+  | King -> true
+  | _ -> (* It's not the king piece we need to find the king piece first *)
+    begin
+    let king = match piece.side with
+      | White -> List.find_exn ~f:(fun x -> Parser.(equal_piece x.piece King)) board.white_pieces
+      | Black -> List.find_exn ~f:(fun x -> Parser.(equal_piece x.piece King)) board.white_pieces
+      | _ -> failwith "Root side is not an option for selected piece!"
+    in
+    let pc, pr = piece.location in
+    let kc, kr = king.location in
+    let pci, kci = (col_to_int pc), (col_to_int kc) in
+    let d = direction_of_delta (pci - kci) (pr - kr) in
+    let rec is_check_direction (loc: location) (avoid: location) (side: Parser.side) (d: direction) (board: plain_board): bool =
+      (* This is the direction check function that examine whether there's check happening in a direction *)
+      (* Avoid is the location of the piece that is about to move *)
+      match loc with
+      | pc, pr -> 
+        (* First check whether on board this square is occupied by a checkable piece *)
+        if equal_location loc avoid then
+          match step_direction 1 d loc with
+          | None -> false
+          | Some loc ->  is_check_direction loc avoid side d board
+        else
+          begin
+          match get_square board pc pr with
+            | Empty _ -> 
+              (* Defer to further squares *)
+              begin
+              match step_direction 1 d loc with
+                | None -> false
+                | Some loc -> is_check_direction loc avoid side d board
+              end
+            | Occupied (_, piece) ->
+              begin
+                if Parser.equal_side piece.side side then
+                  false
+                else
+                  (* Here we know that side are different *)
+                  match d with
+                  | NW -> 
+                    begin
+                      match piece.piece with
+                      | Queen -> true
+                      | Bishop -> true
+                      | _ -> false
+                    end
+                  | NE ->
+                    begin
+                      match piece.piece with
+                      | Queen -> true
+                      | Bishop -> true
+                      | _ -> false
+                    end
+                  | SW ->
+                    begin
+                      match piece.piece with
+                      | Queen -> true
+                      | Bishop -> true
+                      | _ -> false
+                    end
+                  | SE -> 
+                    begin
+                      match piece.piece with
+                      | Queen -> true
+                      | Bishop -> true
+                      | _ -> false
+                    end
+                  | N ->
+                    begin
+                      match piece.piece with
+                      | Rook -> true
+                      | Queen -> true
+                      | _ -> false
+                    end
+                  | S ->
+                    begin
+                      match piece.piece with
+                      | Rook -> true
+                      | Queen -> true
+                      | _ -> false
+                    end
+                  | W ->
+                    begin
+                      match piece.piece with
+                      | Rook -> true
+                      | Queen -> true
+                      | _ -> false
+                    end
+                  | E ->
+                    begin
+                      match piece.piece with
+                      | Rook -> true
+                      | Queen -> true
+                      | _ -> false
+                    end
+              end
+          end
+      in
+    (* One thing we also need to check is that whether the moved piece will remain on the same direction *)
+    begin
+      match mv.target with
+      | None -> (not (is_check_direction king.location piece.location piece.side d board.board))
+      | Some {col=col; row=row} ->
+        let coli = col_to_int col in
+        let dd = direction_of_delta (coli - kci) (row - kr) in
+        (equal_direction dd d) || (not (is_check_direction king.location piece.location piece.side d board.board))
+    end
     end
 
 let check_move_validity (mv: Parser.mv) (piece: piece) (board: board): bool =
@@ -313,44 +462,140 @@ let check_move_validity (mv: Parser.mv) (piece: piece) (board: board): bool =
 
     again we are not checking exclusively only those that allows us to dicern pieces.
   *)
-  | h::tl -> (* First check if h is the desired piece *)
-    if compare_piece_type mv.piece h then
-      (* 
-         Check whether we are able to match further annotation.
-         Notice that if no start_position annotation is given we
-         identify piece with move validity.
-      *)
-      match mv.start_spec with
-      | None -> 
-        begin
-          (* 
-             Unfortunately need to match with move possibility
-          *)
-        end
-      | Some c -> if (Char.(<=) c '8') && (Char.(>=) c '1') then
-        (* Annotation is a row *)
-        begin
-        match h with
-        | {piece=_;location=(_;row);side=_;meta=_} ->
-          (* Now we know that piece matches, lets check whether location matches the annotation *)
-          if (Helper.int_to_char c) = row then
-            Some h
-          else
-            match_move_piece mv tl
-        end
-        else if  (Char.(<=) c 'h') && (Char.(>=) c 'a') then
-          (* Annotation is a row *)
-        begin
-        match h with
-        | {piece=_;location=(col;_);side=_;meta=_} ->
-          if c = col then
-            Some h
-          else
-            match_move_piece mv tl
-        end
-    else 
-      match_move_piece mv tl
+  (check_proper_move piece mv) && (check_no_piece_interference mv piece board.board) && (check_no_discovered_check mv piece board)
 
+let rec update_piece_list (pl: piece list) (from_: location) (to_: location) (promotion: Parser.piece option): piece list =
+  (* This update the piece list with a new piece position *)
+  match pl with
+  | [] -> pl
+  | piece::tl ->
+    if equal_location piece.location from_ then
+      begin
+        let meta = match piece.meta with
+        | Pawn _ ->
+          begin
+            match from_, to_ with
+            (_, fr), (_, tr) ->
+              begin
+                match promotion with
+                | None ->
+                  Pawn {ep=(abs(tr - fr) = 2)}
+                | Some _ -> Other
+              end
+          end
+        | King _ ->
+          King {ck=false; cq=false}
+        | _ -> piece.meta
+        in
+        let piece = {
+          piece=(match promotion with None -> piece.piece | Some prm -> prm);
+          location=piece.location;
+          side=piece.side;
+          meta=meta;
+        } in
+        piece::tl
+      end
+    else
+      piece::(update_piece_list tl from_ to_ promotion)
+
+let rec disable_castling (pl: piece list) (kq: char): piece list = 
+  match pl with
+  | [] -> []
+  | piece::tl ->
+    begin
+      match piece.piece with
+      | King ->
+        {piece=King;
+          location=piece.location;
+          side=piece.side;
+          meta=begin match piece.meta with
+          | King {ck=ck;cq=cq} ->
+            King {ck=(Char.(=) kq 'k') && ck;cq=(Char.(=) kq 'q') && cq}
+          | _ -> failwith "Incorrect meta value!"
+          end
+        }::tl
+      | _ -> piece::(disable_castling tl kq)
+    end
+
+let set_piece_position (from_: location) (to_: location) (board: board) (flip_side: bool) (promotion: Parser.piece option): board =
+  (* This set the piece form position from_ to position to_ *)
+  (* First if we are moving anything from or to a1 or h1 disable castling for white, a8 or h8 for black *)
+  let board =
+    {
+      board=board.board;
+      side_to_play=board.side_to_play;
+      white_pieces=if (equal_location from_ ('a', 1)) || (equal_location to_ ('a', 1)) then disable_castling board.white_pieces 'q' else board.white_pieces;
+      black_pieces=if (equal_location from_ ('a', 8)) || (equal_location to_ ('a', 8)) then disable_castling board.black_pieces 'q' else board.black_pieces;
+    }
+  in
+  let board =
+    {
+      board=board.board;
+      side_to_play=board.side_to_play;
+      white_pieces=if (equal_location from_ ('h', 1)) || (equal_location to_ ('h', 1)) then disable_castling board.white_pieces 'k' else board.white_pieces;
+      black_pieces=if (equal_location from_ ('h', 8)) || (equal_location to_ ('h', 8)) then disable_castling board.black_pieces 'k' else board.black_pieces;
+    }
+  in
+  match from_ with
+  c, r ->
+    match get_square board.board c r with
+    | Empty _ -> failwith "No piece at the starting position!"
+    | Occupied (_, piece) -> 
+      (* construct a new square *)
+      let source_square, target_square = (Empty from_, Occupied (to_, {piece=(match promotion with None -> piece.piece | Some prm -> prm);location=piece.location;side=piece.side;meta=(match promotion with None -> piece.meta | Some _ -> Other)})) in
+      let updated_plain_board = set_square (set_square board.board source_square) target_square in
+      (* Also need to update the piece storage information *)
+      match board.side_to_play with
+      | White -> {
+        board=updated_plain_board;
+        white_pieces=update_piece_list board.white_pieces from_ to_ promotion;
+        black_pieces=board.black_pieces;
+        side_to_play=if flip_side then Black else White;
+      }
+      | Black -> {
+        board=updated_plain_board;
+        white_pieces=board.white_pieces;
+        black_pieces=update_piece_list board.black_pieces from_ to_ promotion;
+        side_to_play=if flip_side then White else Black;
+      }
+      | _ -> failwith "Nothing to do with Root side!"
+
+let rec move_piece (mv: Parser.mv) (pl: piece list) (board: board): board =
+  (* return a piece that can be move for this move *)
+  match pl with
+  | [] -> board
+  | h::tl ->
+    if mv.is_castle_k then
+      (* 
+         We only need to do king-side castle,
+         King -> g1 (g8)
+         Rook -> f1 (f8)
+      *)
+      match board.side_to_play with
+      | White -> set_piece_position ('h', 1) ('f', 1) (set_piece_position ('e', 1) ('g', 1) board false None) true None
+      | Black -> set_piece_position ('h', 8) ('f', 8) (set_piece_position ('e', 8) ('g', 8) board false None) true None
+      | _ -> failwith "Never move for root!"
+    else if mv.is_castle_q then
+      (* We only need to do queen-side castle *)
+      (* 
+         We only need to do king-side castle,
+         King -> c1 (c8)
+         Rook -> d1 (d8)
+      *)
+      match board.side_to_play with
+      | White -> set_piece_position ('a', 1) ('d', 1) (set_piece_position ('e', 1) ('c', 1) board false None) true None
+      | Black -> set_piece_position ('a', 8) ('d', 8) (set_piece_position ('e', 8) ('c', 8) board false None) true None
+      | _ -> failwith "Never move for root!"
+    else if check_move_validity mv h board then
+      (* Move the piece here *)
+      match mv.target with
+      | Some {col=col;row=row} ->
+        begin
+          set_piece_position (h.location) (col, row) board true mv.promote
+        end
+      | None -> failwith "Only castling moves don't have target annotation!"
+    else
+      move_piece mv tl board
 
 let make_move (move: Parser.move) (board: board): board =
   (* 
@@ -360,5 +605,11 @@ let make_move (move: Parser.move) (board: board): board =
   if Parser.equal_side (move.side) (board.side_to_play) then
     (* Should be able to play out the move *)
     let mv = move.move in (* Notice that the piece annotation in mv is of Parser.piece *)
+    let piece_list = match move.side with
+    | White -> board.white_pieces
+    | Black -> board.black_pieces
+    | _ -> failwith "Cannot move feor root side!"
+    in
+    move_piece mv piece_list board
   else 
     failwith "Cannot playout the given move because of playing side conflict!"
